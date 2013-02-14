537bf7e5657f5f7104ad213af63f0ee337730cea
Daniel Wislocki <daniel.wislocki@gmail.com>
2013-02-14 09:56:37 -0500

    Explain job representation's purpose.

--- The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/viewjob/JobRepresentation.java
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/viewjob/JobRepresentation.java
@@ -2,5 +2,6 @@ package com.theladders.solid.isp.viewjob;
 
 public class JobRepresentation
 {
-
+  // This is a stub class that would normally contain fields
+  // that would be serialized into JSON and consumed by a client.
 }
